69f839288699eb80ffbc38a76daf381b44677400
barsikus007 <barsikus07@gmail.com>
2026-03-11 17:56:01 +0300

    feet: move useful functions to flake

--- README.md
+++ README.md
@@ -5,6 +5,6 @@
 1. `yt-dlp 4BGUpWlfFN0 --format bestaudio --output 4BGUpWlfFN0.webm`
    - `ffmpeg -i 4BGUpWlfFN0.webm -ss 7 -to 310 4BGUpWlfFN0.mp3`
 2. `nix run .#serve`
-   - `watchexec --restart --debounce=3000 nix run .#serve`
-      - `watchexec --restart --debounce=3000 nix run .#serve --option substitute false`
+   - `nix run .#serve-watch`
+   - `nix run .#serve-watch-offline`
 3. `nix build && rsync --verbose --archive --copy-links result/ никспобеда.рф:www/никспобеда.рф/`
--- flake.nix
+++ flake.nix
@@ -42,13 +42,31 @@
           imports = [ ./layouts.nix ];
         };
 
-        apps.serve = {
-          type = "app";
-          program =
-            (pkgs.writeShellScript "serve" ''
-              echo "Serving at http://localhost:8080"
-              ${pkgs.python3}/bin/python -m http.server -d ${self.packages.${system}.default} 8080
-            '').outPath;
+        apps = {
+          serve = {
+            type = "app";
+            program =
+              (pkgs.writeShellScript "serve" ''
+                echo "Serving at http://localhost:8080"
+                ${pkgs.python3}/bin/python -m http.server -d ${self.packages.${system}.default} 8080
+              '').outPath;
+          };
+          serve-watch = {
+            type = "app";
+            program =
+              (pkgs.writeShellScript "serve-watch" ''
+                echo "Watching folder"
+                ${pkgs.lib.getExe pkgs.watchexec} --restart --debounce=3000 nix run .#serve
+              '').outPath;
+          };
+          serve-watch-offline = {
+            type = "app";
+            program =
+              (pkgs.writeShellScript "serve-watch-offline" ''
+                echo "Watching folder"
+                ${pkgs.lib.getExe pkgs.watchexec} --restart --debounce=3000 nix run .#serve --option substitute false
+              '').outPath;
+          };
         };
       }
     ));
